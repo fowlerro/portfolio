a18b1f46c0602f24a5df0a196da45b6d422fcb41
fowlerro <fwlerro@gmail.com>
2022-08-08 22:23:57 +0200

    style: add 'glow' effect for illustrations

--- src/components/views/About/Illustration.tsx
+++ src/components/views/About/Illustration.tsx
@@ -5,20 +5,45 @@ import useIsDesktop from '@hooks/useIsDesktop';
 import mobileIllustration from '@assets/illustrations/about_mobile.svg';
 import desktopIllustration from '@assets/illustrations/about_desktop.svg';
 
-const StyledImg = styled('img')({
+const StyledBox = styled('div')(({ theme }) => ({
 	width: '100%',
 	maxWidth: '25rem',
 	marginBottom: '4rem',
-});
+	position: 'relative',
+
+	'::before': {
+		content: "''",
+		position: 'absolute',
+		width: '50%',
+		borderRadius: '50%',
+		aspectRatio: '1/1',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		zIndex: -1,
+		background: 'rgba(255, 255, 255, 0.05)',
+		filter: 'blur(25px)',
+	},
+
+	[theme.breakpoints.up('md')]: {
+		'::before': {
+			width: '100%',
+			filter: 'blur(50px)',
+		},
+	},
+}));
 
 export default function Illustration(): JSX.Element {
 	const isDesktop = useIsDesktop();
 
 	return (
-		<StyledImg
-			src={isDesktop ? desktopIllustration : mobileIllustration}
-			alt="Illustration"
-			aria-hidden
-		/>
+		<StyledBox>
+			<img
+				src={isDesktop ? desktopIllustration : mobileIllustration}
+				width="100%"
+				alt="Illustration"
+				aria-hidden
+			/>
+		</StyledBox>
 	);
 }
--- src/components/views/Contact/Illustration.tsx
+++ src/components/views/Contact/Illustration.tsx
@@ -2,12 +2,31 @@ import { styled } from '@mui/material';
 
 import desktopIllustration from '@assets/illustrations/contact_desktop.svg';
 
-const StyledImg = styled('img')({
+const StyledBox = styled('div')({
 	width: '100%',
 	maxWidth: '20rem',
 	marginBottom: '4rem',
+	position: 'relative',
+
+	'::before': {
+		content: "''",
+		position: 'absolute',
+		width: '100%',
+		borderRadius: '50%',
+		aspectRatio: '1/1',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		zIndex: -1,
+		background: 'rgba(255, 255, 255, 0.1)',
+		filter: 'blur(50px)',
+	},
 });
 
 export default function Illustration(): JSX.Element {
-	return <StyledImg src={desktopIllustration} alt="Illustration" aria-hidden />;
+	return (
+		<StyledBox>
+			<img src={desktopIllustration} alt="Illustration" width="100%" aria-hidden />
+		</StyledBox>
+	);
 }
